a90c50ec8234b3093f87b390bf83857c291a0af9
jvdwijk <joeyvdwijk@gmail.com>
2017-09-26 15:06:32 +0200

    room generator

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class MoveObject
+{
+    public static bool CheckPosition(GameObject room, GameObject[] rooms)
+    {
+        var currentRoomWidth = room.GetComponent<Collider>().bounds.size;
+        var roomPosition = room.transform.position;
+        var roomRect = new Rect(roomPosition,currentRoomWidth);
+        for (var i = 0; i < rooms.Length; i++)
+        {
+            if(room.gameObject == rooms[i].gameObject) continue;
+            var checkingRoomWidth = rooms[i].GetComponent<Collider>().bounds.size;
+            var checkingRoomPosition = rooms[i].transform.position;
+            var checkingRoomRect = new Rect(checkingRoomPosition, checkingRoomWidth);
+            //&& roomPosition.z == checkingRoomPosition.z
+            if (roomRect.Overlaps(checkingRoomRect)) return true;
+        }
+        return false;
+    }
+    
+    public static void MoveRoom(GameObject room)
+    {
+        room.transform.position += new Vector3(0,5,0);
+    }
+}
---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+	[SerializeField] private GameObject[] _roomPrefabs;
+	[SerializeField] private int _roomCount;
+	private GameObject[] Rooms;
+	
+	private void Start () {
+		SpawnRooms();
+		StartCoroutine(RoomSeperator());
+	}
+	
+	private void SpawnRooms ()
+	{
+		Rooms = new GameObject[_roomCount];
+		//Instantiate the given amount of rooms, Choose random rooms from the prefab list.
+		for (var i = 0; i < _roomCount; i++)
+		{
+			Rooms[i] = Instantiate(_roomPrefabs[Random.Range(0, _roomPrefabs.Length)], new Vector3(0, 0, 1), transform.rotation);
+		}
+	}
+
+	private IEnumerator RoomSeperator()
+	{
+		for (var i = 0; i < _roomCount; i++)
+		{
+			print(MoveObject.CheckPosition(Rooms[i], Rooms));
+			while (MoveObject.CheckPosition(Rooms[i], Rooms))
+			{
+				
+				MoveObject.MoveRoom(Rooms[i]);
+				yield return new WaitForSeconds(1);
+			}
+			MoveObject.MoveRoom(Rooms[i]);
+		}
+	}
+	
+	//ToDo: connect Exits to other Exits
+	//ToDo: Move rooms away from each other
+	//ToDo: Make paths to each room.
+}
